622d681d9c77b74b368419a1638a0e5bfe7e26cc
Siddesh Sangodkar <siddesh226@gmail.com>
2018-02-18 18:36:52 +0530

    Added google cloud transcription

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # speech-to-text-
 demo for speech to text using google APIs
+
+* Record audio from browser
+* Stream single channel audio to nodejs server using websocket
+* Send stream for transcription to google API
+
+
+
+
+## Steps to setup
+
+git clone https://github.com/sid226/speech-to-text.git
+
+* Install packages
+```bash 
+npm install
+```
+
+* Setup google credentials
+https://cloud.google.com/speech/docs/reference/libraries
+
+* Add google project details in serve.js
+
+* start server
+```bash 
+node serve.js
+```
+
+
+
+## References:
+
+
+* audio-capture-streaming-to-node-js[https://subvisual.co/blog/posts/39-tutorial-html-audio-capture-streaming-to-node-js-no-browser-extension]
+
--- serve.js
+++ serve.js
@@ -1,6 +1,47 @@
 var binaryServer = require('binaryjs').BinaryServer;
 var wav = require('wav');
 
+
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+
+// Your Google Cloud Platform project ID
+const projectId = 'meetingassist-195320';
+
+// Creates a client
+const client = new speech.SpeechClient({
+  projectId: projectId,
+});
+
+const encoding = 'LINEAR16';
+const sampleRateHertz = 48000;
+const languageCode = 'en-IN';
+
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: true, // If you want interim results, set this to true
+};
+
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : `\n\nReached transcription time limit, press Ctrl+C\n`
+    )
+  );
+
+
 var server = binaryServer({port: 9001});
 console.log("Started server at port: ",9001);
 server.on('connection', function(client) {
@@ -8,15 +49,27 @@ server.on('connection', function(client) {
   var fileWriter = null;
 
 client.on('stream', function(stream, meta) {
-  var fileWriter = new wav.FileWriter('demo.wav', {
+  console.log("piping the stream started!!");
+ 
+  //stream to speech client
+  stream.pipe(recognizeStream);
+
+
+  /* 
+  //Should possibly done on a saparate process thread
+
+   var fileWriter = new wav.FileWriter('demo.wav', {
     channels: 1,
     sampleRate: 48000,
     bitDepth: 16
   });
-  stream.pipe(fileWriter);
+  
+  stream.pipe(fileWriter); */
+  
   stream.on('end', function() {
+    console.log("stream stopped");
     //write audio file
-        fileWriter.end();
+       // fileWriter.end();
   });
 });
 
