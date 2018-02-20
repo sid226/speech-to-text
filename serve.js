98f2607f607c5cda88115894e9b577631f5465a2
Siddesh Sangodkar <siddesh226@gmail.com>
2018-02-20 22:52:00 +0530

    add updated folder structure, save transcription to file

--- serve.js
+++ serve.js
@@ -1,6 +1,17 @@
 var binaryServer = require('binaryjs').BinaryServer;
 var wav = require('wav');
+var fs = require('fs');
+//var Worker = require('webworker-threads').Worker;
 
+
+ROOT_APP_PATH = fs.realpathSync('.'); 
+console.log(ROOT_APP_PATH);
+
+var obj = {
+  table: []
+};
+
+var Transcription=[];
 
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
@@ -50,6 +61,7 @@ function startGoogleSpeechStream(ws) {
     //         : `\n\nReached transcription time limit, press Ctrl+C\n`
     //     )
     console.log("Transcription",data.results[0].alternatives[0].transcript);
+    Transcription.push(data.results[0].alternatives[0].transcript);
   
   });
   return recognizeStream;
@@ -102,7 +114,7 @@ client.on('stream', function(stream, meta) {
       stream.pipe(gstreams[activeStreamID]);
  
             
-   var fileWriter = new wav.FileWriter('demo'+activeStreamID+'.wav', {
+   var fileWriter = new wav.FileWriter(ROOT_APP_PATH+'/clips/'+'demo'+activeStreamID+'.wav', {
     channels: 1,
     sampleRate: 48000,
     bitDepth: 16
@@ -113,6 +125,9 @@ client.on('stream', function(stream, meta) {
   stream.on('end', function() {
     console.log("stream stopped");
    // write audio file
+   
+
+
         fileWriter.end();
   });
 }
@@ -123,6 +138,35 @@ client.on('close', function() {
   console.log("Connection closed!!");
   console.log("END speech stream closed!!");
   gstreams[activeStreamID].end();
+  fs.readFile(ROOT_APP_PATH+'/transcript/script.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+      if(data)
+      {
+        console.log("Data Exists");
+    obj = JSON.parse(data); //now it an object
+      }
+    obj.table.push({id: activeStreamID, text:Transcription}); //add some data
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile(ROOT_APP_PATH+'/transcript/script.json', json, 'utf8', function () {
+      console.log("Transcription written successfully");
+    }); // write it back 
+  }});
+
+  // var fibo = new Worker(function() {
+  //   function fibo (n) {
+  //     return n > 1 ? fibo(n - 1) + fibo(n - 2) : 1;
+  //   }
+  //   this.onmessage = function (event) {
+  //     postMessage(fibo(event.data));
+  //   }
+  // });
+  // fibo.onmessage = function (event) {
+  //   res.end('fib(40) = ' + event.data);
+  // };
+  // fibo.postMessage(40);
+
   if (fileWriter != null) {
     fileWriter.end();
   }
